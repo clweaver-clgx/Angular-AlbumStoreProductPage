9eb92a9fb5269810e0311f0d93a31ff68832e759
Clinton Weaver <clweaver@corelogic.com>
2020-12-14 15:11:00 -0600

    Step3B

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,8 +3,12 @@ import { Http, Response } from '@angular/http'
 
 @Injectable()
 export class ProductService {
-  _albumUrl: '../assets/album.json';
+  private _albumUrl: '../assets/album.json';
 
   constructor(private _http: Http) { }
+
+  getAlbum(id: number){
+    return this._http.get(this._albumUrl);
+  }
 
 }
